1b220eb14a3dc81ee7073a10793e25f10e4e8748
devika <devikadevkasudheer@gmail.com>
2024-07-17 16:20:11 +0530

    file added

---
+++ js/array.js
@@ -0,0 +1,37 @@
+// a=["hai",1,true,null,"hai",2,false,"day",null]
+// console.log(a);
+// console.log(a[6])
+// console.log(a.length)
+// console.log(a.join(" "))
+// console.log(a.indexOf(null))
+// a.splice(a.indexOf("hai"),1)
+// console.log(a)
+// a.push("new")
+// console.log(a)
+// a.pop()
+// console.log(a)
+// console.log()
+
+
+
+num=prompt ("enter the number")
+console.log(num);
+for(i=1;i<=10;i++);
+{
+    if(i%3==0 && i%5==0)
+        {
+        a.push("fizz buzz")
+        }
+        else if(i%5==0)
+            {
+                a.push("buzz")
+            }
+            else if(i%3==0)
+                {
+                    a.push("fizz")
+                }
+                else{
+                    a.push(i)
+                }
+}
+console.log(a);
---
+++ js/string.js
@@ -0,0 +1,18 @@
+str=" hello tea ";
+ console.log(str.length);
+ console.log(str.toUpperCase())
+ console.log(str.toLowerCase())
+ console.log(str.trim())
+ console.log(str.trimLeft())
+ console.log(str.trimRight())
+ console.log(str.substring(2,4));
+ console.log(str.replace("hello","hai"))
+ console.log(str.startsWith("helo"))
+ console.log(str.endsWith("tea"))
+ console.log(str.())
+ 
+ 
+ 
+ 
+ 
+ 
